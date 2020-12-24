18d1db26dbc055d43b836c97fbbb8429d8d4c2a2
interesting926 <995941767@qq.com>
2020-12-24 16:13:53 +0800

    send mqtt ok

---
+++ src/main/java/com/example/myaito/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.example.myaito.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    MqttGateway mqttGateway;
+    @RequestMapping("/sendMqttMessage")
+    public String sendMqttMessage(String message, String topic) {
+        mqttGateway.sendToMqtt(message, topic);
+        return "ok";
+    }
+}
---
+++ src/test/java/com/example/myaito/MyaitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myaito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
